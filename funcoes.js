c8645ce2bafdda1503f9d3a45cfdb7d39935bc7b
Victor-AOliveira <xdviihs2@hotmail.com>
2020-03-26 21:48:47 -0300

    atualizando

--- funcoes.js
+++ funcoes.js
@@ -1,12 +1,60 @@
+/*
 alert("Aula de JavaScript");
 //sempre finalizar com ; (comentario de 1 linha //)
-/*
+
 comentario com varias linhas
 */
 
 
 //variaveis:
-var num = 0; // atribuir numero para variavel numerica
+/*var num = 2020; // atribuir numero para variavel numerica
 var nome = "Victor"; // variaveis com letras aspas duplas
 var carac = ''; // variaveis com um caracter aspas simples
-alert(nome)
+var x = "Noite ";
+var y = "Dia";
+var teste = x + y;
+
+alert(teste);*/
+
+//Funçoes
+var x = 0;
+var y = 1;
+// if sempre com parenteses e chaves
+if(x==y){
+    alert("Iguais");//comandos dentro das chaves
+     
+}
+
+
+/*else if (x>y){
+    alert("Maior");  sempre que tiver um else if sempre finalizar com um else
+}*/
+else{
+    alert("Diferentes");
+}
+
+while(x <= 10){
+    alert(x);
+    x+=2;
+}
+do{
+   alert(x);
+}while(x <= 10)
+/*
+x+=2 some dois em dois numeros
+x++ sempre soma +1 no valor
+x-- sempre subtrair -1 no valor
++ soma
+- subtração
+/ divisão
+* multiplicação
+% resto de divisão 
+
+== igual
+!= diferente
+> Maior
+< menor
+<= menor igual
+>= maior igual 
+*/
+
