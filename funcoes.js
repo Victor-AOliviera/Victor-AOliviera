6a62ab2ec449fece961e6ab11655bee7eb01339a
Victor-AOliveira <xdviihs2@hotmail.com>
2020-03-26 21:12:27 -0300

    atualizando javascript

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+alert("Aula de JavaScript");
+//sempre finalizar com ; (comentario de 1 linha //)
+/*
+comentario com varias linhas
+*/
+
+
+//variaveis:
+var num = 0; // atribuir numero para variavel numerica
+var nome = "Victor"; // variaveis com letras aspas duplas
+var carac = ''; // variaveis com um caracter aspas simples
+alert(nome)
